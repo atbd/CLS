90a6a54ff6b78a3d79565c4a1273271d51bbe7db
Anthony Delannoy <anthony.2lannoy@gmail.com>
2015-02-05 09:35:37 +0100

    Lecture optimisation + séparation ecriture

---
+++ Python/RWFormats/ecriture.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*-coding:utf-8 -*
+
+#####################################################
+#####################################################
+                ### PARTIE ÉCRITURE ###
+#####################################################
+#####################################################
+
+
+def ecritureUneTransmission(data): # pas encore testé
+
+    """ data (dictionnaire) doit contenir :
+            - l'identifiant, date, heure, LC et IQ
+            - lat1, lat2, lon1, lon2
+            - nbr message + >120dB + bestLevel
+            - Pass duration et NOPC
+            - freq et altitude
+            - autres chiffres ?
+    """
+    firstLine = " %s  Date : %s %s  LC : %s  IQ : %s \n" % (data["identifiant"], data["date"], data["heure"], data["LC"], data["IQ"])
+
+    secondLine = "      Lat1 : %s  Lon1 : %s  Lat2 : %s  Lon2 : %s \n" % (data["lat1"], data["lon1"], data["lat2"], data["lon2"])
+
+    thirdLine = "      Nb mes : %s  Nb mes>-120dB : %s  Best level : %s dB\n" % (data["nbrMess"], data["nbMessSupp120dB"], data["bestdB"])
+
+    fourthLine = "      Pass duration : %ss  NOPC : %s\n" % (data["passDuration"], data["NOPC"])
+    
+    fifthLine = "      Calcul freq : %s Hz   Altitude :   %s m\n" % (data["freq"], data["altitude"])
+    
+    sixthLine = "                 00          00          00          00\n" # sais pas encore
+    
+    total = "\n" + firstLine + secondLine + thirdLine + fourthLine + fifthLine + sixthLine
+
+    return total
+
+
+
+def ecritureDIAG(liste, nomFichier): # pas encore testé
+
+    """
+        Cette fonction écrira les données sous format ARGOS dans un .DIAG (nommé selon l'identifiant de la tortue).
+        liste est la liste contenant tous les dictionnaires de transmission.
+    """
+    fichier = "../../DIAG/" # voir comment ça se passe si on appelle la fonction depuis un autre .py dans un autre dossier. Si ça marche pas, mettre le chemin en argument.
+
+    with open(fichier + nomFichier, "w") as f:
+        for i in liste:
+            tmp = ecritureUneTransmission(liste[i])
+            f.write(tmp)
--- Python/RWFormats/lecture.py
+++ Python/RWFormats/lecture.py
@@ -14,10 +14,12 @@
 """
 
 def lectureToutDiag(pathDiagFile, liste):
-    # pathDiagFile est le chemin d'accès au fichier .DIAG
-    # data est un dictionnaire qui contiendra les données d'une
-    #   seul transmission
-    # liste contiendra tous les dictionnaires
+    """
+        # pathDiagFile est le chemin d'accès au fichier .DIAG
+        # data est un dictionnaire qui contiendra les données d'une
+        #   seul transmission
+        # liste contiendra tous les dictionnaires
+    """
 
     with open(pathDiagFile, "r") as f:
 
@@ -81,8 +83,11 @@ def lectureUnCSV(pathCSVFile, liste): # pas encore testé
 
         for row in f:   # voir encadrant pour info sur ceux commentés + les données dans CSV que j'ai pas pris
             
+            if row == []:
+                continue
+
             data = {}
-            
+
             row = filter(None, row)
             
             data["date"] = row[1]
@@ -104,14 +109,14 @@ def lectureUnCSV(pathCSVFile, liste): # pas encore testé
             liste.append(data)
 
 
-def lectureDossier(folderPath, viveLesLuth, choix):
-    # viveLesLuth contiendra un dictionnaire dont chaque entrée, identifié par l'identifiant 
-    # de l'émetteur, contiendra la liste correspondante (générée par lectureToutDiag())
-    #
-    # folderPath est le chemin du répertoire contenant tous les .DIAG 
-    # NB : ne pas oublier le "/" à la fin de folderPath
-    #
-    # choix pour le type de fichier
+def lectureDossier(folderPath, viveLesLuth):
+    """
+        # viveLesLuth contiendra un dictionnaire dont chaque entrée, identifié par l'identifiant 
+        # de l'émetteur, contiendra la liste correspondante (générée par lectureToutDiag())
+        #
+        # folderPath est le chemin du répertoire contenant tous les .DIAG 
+        # NB : ne pas oublier le "/" à la fin de folderPath
+    """
 
     from os import listdir
 
@@ -121,63 +126,26 @@ def lectureDossier(folderPath, viveLesLuth, choix):
         
         filePath = folderPath + Files[num-1]
         tmp = []
-        lectureToutDiag(filePath, tmp)
 
-        if choix == "diag":
+        Format = Files[num-1].split(".")[-1].upper()
+
+        if  Format == "DIAG":
+            lectureToutDiag(filePath, tmp)
             identifiant = Files[num-1].split(".")[0]
-        elif choix == "csv":
+
+        elif Format == "DS":
+            # à faire
+            continue
+
+        elif Format == "CSV":
+            lectureUnCSV(filePath, tmp)
             identifiant = Files[num-1].split("-")[0]
+            
+        elif Format == "XML":
+            # à faire
+            continue
 
         viveLesLuth[identifiant] = tmp
-
-
-#####################################################
-#####################################################
-                ### PARTIE ÉCRITURE ###
-#####################################################
-#####################################################
-
-
-def ecritureUneTransmission(data): # pas encore testé
-
-    """ data (dictionnaire) doit contenir :
-            - l'identifiant, date, heure, LC et IQ
-            - lat1, lat2, lon1, lon2
-            - nbr message + >120dB + bestLevel
-            - Pass duration et NOPC
-            - freq et altitude
-            - autres chiffres ?
-    """
-    firstLine = " %s  Date : %s %s  LC : %s  IQ : %s \n" % (data["identifiant"], data["date"], data["heure"], data["LC"], data["IQ"])
-
-    secondLine = "      Lat1 : %s  Lon1 : %s  Lat2 : %s  Lon2 : %s \n" % (data["lat1"], data["lon1"], data["lat2"], data["lon2"])
-
-    thirdLine = "      Nb mes : %s  Nb mes>-120dB : %s  Best level : %s dB\n" % (data["nbrMess"], data["nbMessSupp120dB"], data["bestdB"])
-
-    fourthLine = "      Pass duration : %ss  NOPC : %s\n" % (data["passDuration"], data["NOPC"])
-    
-    fifthLine = "      Calcul freq : %s Hz   Altitude :   %s m\n" % (data["freq"], data["altitude"])
-    
-    sixthLine = "                 00          00          00          00\n" # sais pas encore
-    
-    total = "\n" + firstLine + secondLine + thirdLine + fourthLine + fifthLine + sixthLine
-
-    return total
-
-
-
-def ecritureDIAG(liste, nomFichier): # pas encore testé
-
-    """
-        Cette fonction écrira les données sous format ARGOS dans un .DIAG (nommé selon l'identifiant de la tortue).
-        liste est la liste contenant tous les dictionnaires de transmission.
-    """
-    fichier = "../../DIAG/" # voir comment ça se passe si on appelle la fonction depuis un autre .py dans un autre dossier. Si ça marche pas, mettre le chemin en argument.
-
-    with open(fichier + nomFichier, "w") as f:
-        for i in liste:
-            tmp = ecritureUneTransmission(liste[i])
-            f.write(tmp)
 
 
 
